839246b2f3a9792918a5e5fb9b19fc8a348189e4
root <root@ip-172-31-24-207.ec2.internal>
2022-06-03 12:54:51 +0000

    modified cmd file

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,3 @@
 nohup dockerd &
 sleep 30
-for i in 1 2; do docker run -d  --privileged --security-opt="apparmor=docker-nginx-sample" -p 800$i:80 --name=apparmor-$i jayaprakashsaranya/kkimages:apparmnginx sh -c 'sleep infinity';docker exec apparmor-$i sh -c 'service apparmor restart;apparmor_status';done;sleep infinity
-service apparmor restart
-apparmor_status
+for i in 1 2; do docker run -d  --privileged --security-opt="apparmor=docker-nginx-sample" -p 800$i:80 --name=apparmor-$i jayaprakashsaranya/kkimages:apparmnginx sh -c 'sleep infinity';docker exec apparmor-$i sh -c 'service apparmor restart;apparmor_status';service apparmor restart;apparmor_status;done;sleep infinity
