16bf78a737a93ed7fc3f7e30f3408fde0a76a987
root <root@ip-172-31-24-207.ec2.internal>
2022-06-03 12:40:32 +0000

    added files for task 2 restry

---
+++ cmd.sh
@@ -0,0 +1,5 @@
+nohup dockerd &
+sleep 30
+for i in 1 2; do docker run -d  --privileged --security-opt="apparmor=docker-nginx-sample" -p 800$i:80 --name=apparmor-$i jayaprakashsaranya/kkimages:apparmnginx sh -c 'sleep infinity';docker exec apparmor-$i sh -c 'service apparmor restart;apparmor_status';done;sleep infinity
+service apparmor restart
+apparmor_status
